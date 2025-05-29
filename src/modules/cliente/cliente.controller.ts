19c868c1c594935907991f0f12ea30fefbe1ecad
EdwinCrespo <e.crespoparada@gmail.com>
2025-05-29 01:06:15 -0400

    Cliente

--- src/modules/cliente/cliente.controller.ts
+++ src/modules/cliente/cliente.controller.ts
@@ -1,33 +1,38 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete,UseGuards } from '@nestjs/common';
 import { ClienteService } from './cliente.service';
 import { CreateClienteDto } from './dto/create-cliente.dto';
 import { UpdateClienteDto } from './dto/update-cliente.dto';
-
+import { JwtAuthGuard } from '../../auth/guards/jwt-auth.guard';
 @Controller('cliente')
 export class ClienteController {
   constructor(private readonly clienteService: ClienteService) {}
 
   @Post()
+  @UseGuards(JwtAuthGuard)
   create(@Body() createClienteDto: CreateClienteDto) {
     return this.clienteService.create(createClienteDto);
   }
 
-  @Get()
-  findAll() {
-    return this.clienteService.findAll();
+  @Get(':id')
+  @UseGuards(JwtAuthGuard)
+  findAll(@Param('id') id: string) {
+    return this.clienteService.findAll(id);
   }
 
-  @Get(':id')
+  @Get('detalle/:id')
+  @UseGuards(JwtAuthGuard)
   findOne(@Param('id') id: string) {
     return this.clienteService.findOne(+id);
   }
 
   @Patch(':id')
+  @UseGuards(JwtAuthGuard)
   update(@Param('id') id: string, @Body() updateClienteDto: UpdateClienteDto) {
     return this.clienteService.update(+id, updateClienteDto);
   }
 
   @Delete(':id')
+  @UseGuards(JwtAuthGuard)
   remove(@Param('id') id: string) {
     return this.clienteService.remove(+id);
   }
--- src/modules/cliente/cliente.service.ts
+++ src/modules/cliente/cliente.service.ts
@@ -1,26 +1,95 @@
 import { Injectable } from '@nestjs/common';
 import { CreateClienteDto } from './dto/create-cliente.dto';
 import { UpdateClienteDto } from './dto/update-cliente.dto';
-
+import { PrismaService } from '../../prisma/prisma.service';
 @Injectable()
 export class ClienteService {
+  constructor(private readonly prisma: PrismaService) {}
+
   create(createClienteDto: CreateClienteDto) {
-    return 'This action adds a new cliente';
+    try {
+      createClienteDto.estado = 1;
+      return this.prisma.cliente.create({
+        data: createClienteDto,
+      });
+    } catch (error) {
+      throw new Error('Error al crear la cliente');
+    }
   }
 
-  findAll() {
-    return `This action returns all cliente`;
+  findAll(id: string) {
+    try {
+      // cuando el estado sea 1, es decir, activo
+      return this.prisma.cliente.findMany({
+        where: {
+          estado: 1,
+          usuarioId:id,
+        },
+      });
+    } 
+    catch (error) {
+      throw new Error('Error al obtener las cliente');
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} cliente`;
+  async findOne(id: number) {
+    try {
+      const cliente = await this.prisma.cliente.findUnique({
+        where: { id },
+      });
+
+      if (!cliente) {
+        throw new Error(`Cleinte con ID ${id} no encontrado`);
+      }
+
+      return cliente;
+    } catch (error) {
+      if (error instanceof Error) {
+        throw error;
+      }
+      throw new Error(`Error al obtener Cliente: ${error.message}`);
+    }
   }
 
-  update(id: number, updateClienteDto: UpdateClienteDto) {
-    return `This action updates a #${id} cliente`;
+  async update(id: number, UpdateClienteDto: UpdateClienteDto) {
+    try {
+      UpdateClienteDto.fechaActualizacion=new Date();
+      const cliente = await this.prisma.cliente.update({
+        where: { id },
+        data: UpdateClienteDto,
+      });
+
+      if (!cliente) {
+        throw new Error(`Cliente con ID ${id} no encontrado`);
+      }
+
+      return cliente;
+    } catch (error) {
+      if (error instanceof Error) {
+        throw error;
+      }
+      throw new Error(`Error al actualizar cliente: ${error.message}`);
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} cliente`;
+ async remove(id: number) {
+    try {
+      // actualizar estado a 0 (inactivo)
+      const cliente = await this.prisma.cliente.update({
+        where: { id },
+        data: { estado: 0 },
+      });
+
+      if (!cliente) {
+        throw new Error(`Cliente con ID ${id} no encontrado`);
+      }
+
+      return cliente;
+    } catch (error) {
+      if (error instanceof Error) {
+        throw error;
+      }
+      throw new Error(`Error al actualizar cliente: ${error.message}`);
+    }
   }
 }
